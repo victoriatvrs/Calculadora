3e40e47f79202778f041a755408a82b0e08c35ed
victoria tavares <victoriaspina08@gmail.com>
2026-05-28 23:03:16 -0300

    feat: add power feature

--- calculadora.py
+++ calculadora.py
@@ -5,6 +5,7 @@ print("\n\n[+] Soma")
 print("\n[-] Subtração")
 print("\n[*] Multiplicação")
 print("\n[/] Divisão")
+print("\n[ˆ] Potência")
 operacao = input("\nEscolha a operação: ")
 
 if operacao == "+":
@@ -22,6 +23,12 @@ elif operacao == "*":
 elif operacao == "/":
   result = n1 / n2
   print(f"{n1} / {n2} = {result}")
+  
+elif operacao == "ˆ":
+  result = 1
+  for _ in range(int(n2)):
+    result = result * n1
+  print(f"{n1}ˆ{n2} = {result}")
   
 else:
   print("Operação inválida!")
