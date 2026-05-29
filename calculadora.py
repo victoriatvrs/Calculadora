051ae87d7f1a6e285b7a82e6d64eb759bd576489
victoria tavares <victoriaspina08@gmail.com>
2026-05-28 23:21:25 -0300

    fix: number menu working

--- calculadora.py
+++ calculadora.py
@@ -11,29 +11,29 @@ print("\n[5] Potência")
 print("\n[6] Raiz quadrada (usado apenas o primeiro número)")
 operacao = input("\nEscolha a operação: ")
 
-if operacao == 1:
+if operacao == '1':
   result = n1 + n2
   print(f"{n1} + {n2} = {result}")
   
-elif operacao == 2:
+elif operacao == '2':
   result = n1 - n2
   print(f"{n1} - {n2} = {result}")
   
-elif operacao == 3:
+elif operacao == '3':
   result = n1 * n2
   print(f"{n1} * {n2} = {result}")
   
-elif operacao == 4:
+elif operacao == '4':
   result = n1 / n2
   print(f"{n1} / {n2} = {result}")
   
-elif operacao == 5:
+elif operacao == '5':
   result = 1
   for _ in range(int(n2)):
     result = result * n1
   print(f"{n1}ˆ{n2} = {result}")
   
-elif operacao == 6:
+elif operacao == '6':
   result = math.sqrt(n1)
   print(f"Raiz quadrada de {n1} = {result}")
   
